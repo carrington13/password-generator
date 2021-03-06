096d3d822a43304a49e13b35762be0894baa76b6
Mediogre <caseyarrington13@gmail.com>
2021-03-06 17:34:34 -0600

    greatly refactored code and fixed bug: acceptance of zero selected character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,78 +3,78 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// prompt user for desired password criteria and save it to a variable
-var useUpperCase = confirm("Use uppercase letters?");
-var useLowerCase = confirm("Use lowercase letters?");
-var useSpecial = confirm("Use special characters?");
-var useNumber = confirm("Use numbers?");
-var chosenPassLengthString = prompt("How many characters do you want your password to be?");
-let characters = "";
-let chosenPassLength = parseInt(chosenPassLengthString);
-console.log(chosenPassLengthString);
-// Arrays for all potential characters by type
-var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCaseLetters = "abcdefghijklmnopqurstuvwxyz";
-var numbers = "1234567890";
-var special = "~!@#$%^&*()_+{}|`=-[];/<>,.?"
-let password = "";
+// Array of Objects to hold respective password character values and user selection 
+let passCriteria = [
+    upperCase = {"characters": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",},
+    lowerCase = {"characters": "abcdefghijklmnopqurstuvwxyz",},
+    numbers = {"characters": "1234567890",},
+    // (Biased Decision) Separate special characters into two objects based on use preference.
+    specialOne = {"characters": "~!@?#$%=+",},
+    specialTwo = {"characters": "`[]{}|<>/-(^)*:;'",},
+]
 
+// Creates variable of textarea element
 let passwordEl = document.querySelector("#password");
-// Function to check chosenPassLength's validity
-var passLengthCheck = function() {
 
-    if (chosenPassLength < 8 || !chosenPassLength) {
-            chosenPassLength = prompt("Your password must be at least 8 characters. Please try again.");
+// function for prompts
+let promptUser = function() { 
 
-            passLengthCheck();
-        } else if (chosenPassLength > 128) {
-            chosenPassLength = prompt("Your password must be fewer than 128 characters. Please try again");
-            
-            passLengthCheck();
-
-        } else if (isNaN(chosenPassLength)) {
-            chosenPassLength = prompt("The input must be a number. Please try again.")
-            passLengthCheck();
-        }
-        // return chosenPassLength;
-    }
-
-passLengthCheck();
+    // prompt user for character selection by type and capture/push response to relevant object.use in passCritera array.
+    upperCase.use = confirm("Use uppercase letters?");
+    lowerCase.use = confirm("Use lowercase letters?");
+    numbers.use = confirm("Use numbers?");
+    specialOne.use = confirm("Use special characters:( ~!@?#$%&=+ )?");
+    specialTwo.use = confirm("Use special characters:( `[]{}|<>/-(^)*:;' )?");
+}
 
 // Event Listener for generateBtn
 generateBtn.addEventListener("click", function() {
-    // if useUpperCase is true
-    if (useUpperCase) {
-        // add upperCaseLetters to characters
-        characters += upperCaseLetters;  
-    }  
-    // if useLowerCase is true
-    if (useLowerCase) {
-        // add lowerCaseLetters to characters
-        characters += lowerCaseLetters;
+
+    // Declare and reset password for each password generation
+    let password = "";
+    let passCharacters = "";
+    
+    // Prompt User for Password Criteria by calling promptUser function
+    promptUser();
+    
+    // Verify at least one character type is chosen by
+    // looping promptUser call until at least one is true 
+    while (!upperCase.use && !lowerCase.use && !numbers.use && !specialOne.use && !specialTwo.use) {
+        // if not valid, tell user they need to pick at least one and ...
+        alert("You must select at least one option for your password! Please try again.");
+        // call promptUser function again
+        promptUser();
+    }
+        
+    // Prompt user for password length
+    let chosenPassLength = parseInt(prompt("How many characters do you want your password to be?"));
+    
+    // Verify user input for chosenPassLength 
+    while (chosenPassLength < 8 || chosenPassLength > 128 || isNaN(chosenPassLength) || !chosenPassLength) {
+        // if value is unacceptable, prompt them until it is...
+        chosenPassLength = parseInt(prompt("The password length must be a number between 8 and 128. Please try again."));
     }
 
-    // if useNumbers is true
-    if (useNumber) {
-        // add numbers to characters
-        characters += numbers; 
-    }
-    // if useSpecial is true
-    if (useSpecial) {
-        // add special to characters
-        characters += special;
-    }
-
-    console.log(characters);
-
-    //pass word generation
-    for (var i = 0; i < chosenPassLength; i++) {
-        let randomChar = characters[Math.floor(Math.random() * characters.length)];
-        password += randomChar
-        console.log(randomChar);
+    // loop through array passCriteria
+    for (let i = 0; i < passCriteria.length; i++) {
+        
+        // if passCriteria object.use = true
+        if (passCriteria[i].use) {
+            // add value of object.characters to passCharacters
+            passCharacters += passCriteria[i].characters;
+        }
     }
 
-    console.log(password);
+    //password generation
+    for (let i = 0; i < chosenPassLength; i++) {
+        // Declare variable by selecting a random single character from passCharacters string 
+        let randomChar = passCharacters[Math.floor(Math.random() * passCharacters.length)];
+        // and add randomChar value to password string.
+        password += randomChar;
+    }
+
+    // print generated password to screen
     passwordEl.textContent = password;
 
-})
+}
+)
